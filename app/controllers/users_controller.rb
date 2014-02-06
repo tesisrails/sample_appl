418241818b3a4c1b9587bf6fdef8d087ecdb8072
Pavel <alfredo.pcp@ciencias.unam.mx>
2014-02-06 16:48:53 -0600

    Alpha 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
   def show
 	@user = User.find(params[:id])
   end
@@ -9,11 +12,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-	@user = User.find(params[:id])
+	
   end
   
   def update
-	@user = User.find(params[:id])
 	if @user.update_attributes(user_params)
 		flash[:success] = "Profile Updated"
 		redirect_to @user
@@ -32,9 +34,24 @@ class UsersController < ApplicationController
 		render 'new'
 	end
   end
-	private 
 	
-		def user_params
-			params.require(:user).permit(:name, :email, :password, :password_confirmation)
-		end
+  #-------------------------------------
+  
+  private 
+	
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
+	
+	#Before FFiltros
+	
+	def signed_in_user
+		redirect_to signin_url, notice: "Please sign in." unless signed_in?
+	end
+	
+	def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
+

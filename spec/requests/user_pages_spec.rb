a1261f57ae5788838d5bbe7267f307cc41365f27
Pavel <alfredo.pcp@ciencias.unam.mx>
2014-02-05 18:10:54 -0600

    Seguimos con autorización

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,7 +4,7 @@ describe "User pages" do
 
   subject { page }
 
-describe "profile page" do
+  describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
@@ -19,7 +19,7 @@ describe "profile page" do
     it { should have_title(full_title('Sign up')) }
   end
   
- describe "signup page" do
+  describe "signup page" do
 
     before { visit signup_path }
 
@@ -51,6 +51,39 @@ describe "profile page" do
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
+    end
+  end
+  
+  describe "edit" do
+	let(:user) { FactoryGirl.create(:user) }
+	
+	before do
+		sign_in user
+		visit edit_user_path(user) 
+	end
+	
+	describe "page" do
+		it { should have_content("Update your profile") }
+		it { should have_title("Edit user") }
+		it { should have_link('change', href: 'http://gravatar.com/emails') }
+	end
+	
+	describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { expect(user.reload.name).to  eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
     end
   end
 end

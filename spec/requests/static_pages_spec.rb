c4363a309a6e2bb4d0b12cdc7e9fcddac6a2f0de
Pavel <alfredo.pcp@ciencias.unam.mx>
2014-02-19 19:20:24 -0600

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -20,13 +20,18 @@ describe "Static pages" do
         visit root_path
       end
 
-      it "should render the user's feed" do
-        user.feed.each do |item|
-          expect(page).to have_selector("li##{item.id}", text: item.content)
+     describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
         end
-      end
-    end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+     end
   end
+ end
   
 
   describe "Help page" do
